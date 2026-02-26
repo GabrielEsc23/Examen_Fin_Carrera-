24781c87acea3a1bcfd52213a8fc511ff0bd9eec
Gabriel Escobar <gabrielescobar283@gmail.com>
2026-02-26 10:26:13 -0500

    Se creo el endpoint para eliminar y actualizar pacientes  por id

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -60,8 +60,46 @@ const detallePaciente = async (req, res) => {
 
 }
 
+
+// Método para actualizar un paciente (CRUD - Update)
+const actualizarPaciente = async (req, res) => {
+    const { id } = req.params
+
+    if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos" })
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ msg: `Lo sentimos, no existe el paciente con ID ${id}` })
+    }
+
+    try {
+        await Paciente.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ msg: "Paciente actualizado con éxito" })
+    } catch (error) {
+        res.status(500).json({ msg: "Error al actualizar al paciente" })
+    }
+}
+
+
+// Método para eliminar (dar de baja) un paciente (CRUD - Delete)
+const eliminarPaciente= async (req, res) => {
+    const { id } = req.params
+
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ msg: `Lo sentimos, no existe el Paciente con ID ${id}` });
+    }
+
+    try {
+        await Paciente.findByIdAndDelete(id);
+        res.status(200).json({ msg: "Paciente eliminado con éxito" });
+    } catch (error) {
+        res.status(500).json({ msg: "Error al eliminar al Paciente" });
+    }
+}
 export{
     registrarPaciente,
     listarPaciente,
-    detallePaciente
+    detallePaciente,
+    actualizarPaciente,
+    eliminarPaciente
 }
--- src/routers/paciente_routes.js
+++ src/routers/paciente_routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
-import { registrarPaciente,listarPaciente,detallePaciente } from '../controllers/paciente_controller.js';
+import { registrarPaciente,listarPaciente,detallePaciente, actualizarPaciente, eliminarPaciente } from '../controllers/paciente_controller.js';
 import { verificarAutenticacion } from '../middlewares/autenticacion.js';
 
 const router = Router()
 router.post('/paciente/registrar',verificarAutenticacion,registrarPaciente)
 router.get('/paciente/listar',verificarAutenticacion,listarPaciente)
 router.get('/paciente/detalle/:id',verificarAutenticacion,detallePaciente)
+router.put('/paciente/actualizar/:id',verificarAutenticacion,actualizarPaciente)
+router.delete('/paciente/eliminar/:id',verificarAutenticacion,eliminarPaciente)
 
 export default router
