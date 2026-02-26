ef1cd7139679fc1091d653708427e69a80ebcaa8
Gabriel Escobar <gabrielescobar283@gmail.com>
2026-02-26 11:11:42 -0500

    Se creo el endpoint para actualizar especialidad

--- src/controllers/especialidad_controller.js
+++ src/controllers/especialidad_controller.js
@@ -23,6 +23,71 @@ const registrarEspecialidad= async (req, res) => {
         res.status(500).json({ msg: "Error al registrar la especialidad" })
     }
 }
+
+// Método para listar todos los auditorios
+const listarEspecialidades = async (req, res) => {
+    try {
+        const especiaslidades = await Especialidad.find().select("-__v")
+        res.status(200).json(especiaslidades)
+    } catch (error) {
+        res.status(500).json({ msg: "Error al obtener Especialidades" })
+    }
+}
+
+
+// Método para ver el detalle de una especialidad en particular
+const detalleEspecialidad= async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ msg: `Lo sentimos, no existe el especialidad con ID ${id}` });
+    }
+
+    try {
+        const especialidad= await Especialidad.findById(id).select("-__v")
+        res.status(200).json(especialidad)
+    } catch (error) {
+        res.status(500).json({ msg: "Error al obtener especialidad" })
+    }
+}
+
+// Método para actualizar una especialidad
+const actualizarEspecialidad = async (req, res) => {
+    const { id } = req.params
+
+    if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos" })
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ msg: `Lo sentimos, no existe la especialidad con ID ${id}` })
+    }
+
+    try {
+        await Especialidad.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ msg: "Especialidad actualizado con éxito" })
+    } catch (error) {
+        res.status(500).json({ msg: "Error al actualizar pla especilidad" })
+    }
+}
+
+// Método para eliminar una 
+const eliminarEspecialidad = async (req, res) => {
+    const { id } = req.params;
+
+    if (Object.values(req.body).includes("")) return res.status(400).json({ msg: "Lo sentimos, debes llenar todos los campos" })
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ msg: `Lo sentimos, no existe el auditorio con ID ${id}` })
+    }
+
+    try {
+        await Auditorio.findByIdAndDelete(id);
+        res.status(200).json({ msg: "Auditorio eliminado con éxito" })
+    } catch (error) {
+        res.status(500).json({ msg: "Error al eliminar el auditorio" })
+    }
+}
 export{
-    registrarEspecialidad
+    registrarEspecialidad,
+    listarEspecialidades,
+    detalleEspecialidad,
+    actualizarEspecialidad
 }
--- src/routers/especialidad_routes.js
+++ src/routers/especialidad_routes.js
@@ -1,9 +1,14 @@
 import {Router} from 'express';
-import { registrarEspecialidad } from '../controllers/especialidad_controller.js';
+import { actualizarEspecialidad, detalleEspecialidad, listarEspecialidades, registrarEspecialidad } from '../controllers/especialidad_controller.js';
 import { verificarAutenticacion } from '../middlewares/autenticacion.js';
 
 const router = Router()
 
 router.post('/especialidad/registrar',verificarAutenticacion,registrarEspecialidad)
+router.get('/especialidad/listar',verificarAutenticacion,listarEspecialidades)
+router.get('/especialidad/detalle/:id',verificarAutenticacion,detalleEspecialidad)
+router.put('/especialidad/actualizar/:id',verificarAutenticacion,actualizarEspecialidad)
+
+
 
 export default router
